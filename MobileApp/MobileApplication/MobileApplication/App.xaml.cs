774f3cb8a219368aa7c24b2730b1be35ebf9ad05
MORGAN-JUNIOR\Morgan <mojaquith@gmail.com>
2019-09-30 13:19:19 -0400

    -Login, Create Account, Front Page

---
+++ MobileApp/MobileApplication/MobileApplication/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MobileApplication.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MobileApplication
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new FrontPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
