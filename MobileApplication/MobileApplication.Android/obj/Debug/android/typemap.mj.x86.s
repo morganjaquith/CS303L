edd68dde8c0ba4309fbe5a8eaed381d722779583
CamSchleicher <cameron.schleicher@snhu.edu>
2019-11-04 13:15:59 -0500

    Barcode Changes

---
+++ MobileApplication/MobileApplication.Android/obj/Debug/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 0aecae44579cd3dcf35483fcc19fc1526600737d */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1324
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 346889
+	.include	"typemap.mj.inc"
